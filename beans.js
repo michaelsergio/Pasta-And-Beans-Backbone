c3ca43fefe6503028eb004a0587a4adea1c075f2
Michael Sergio <mikeserg@gmail.com>
2012-06-12 17:34:34 -0400

    refactored names for sleep

--- beans.js
+++ beans.js
@@ -10,13 +10,16 @@ var SleepItem = Backbone.Model.extend({
   
 }); 
 
-var SleepCollection = Backbone.Collection.extend({
+var SleepLog = Backbone.Collection.extend({
   model: SleepItem,
   localStorage: new Store("sleep-backbone")
 });
 
 var SleepView = Backbone.View.extend({
   el: $('#sleep-app'),
+
+  tagName: "li", 
+  className: "sleep-row",
   
   initialize: function() { 
     this.input_start_date  = this.$("#start-date");
@@ -25,17 +28,22 @@ var SleepView = Backbone.View.extend({
     this.input_end_time = this.$("#end-time");
     this.time_between = this.$("#time-between");
 
+    this.inputReset();
+    this.showTimeDiffInWords();
+  },
+
+  inputReset: function() {
     var today = Date.now();
     this.input_start_date.val(today.getMonth() +"/"+ today.getDay()+"/"+ today.getFullYear()); 
     this.input_end_date.val(today.getMonth() +"/"+ today.getDay()+"/"+ today.getFullYear()); 
     this.input_start_time.val("22:00");
     this.input_end_time.val("8:00");
-    this.calcTimeBetween();
+  
   },
 
   events: {
     //"keypress .edit": "updateOnEnter",
-    "blur #date-boxes": "calcTimeBetween",
+    "blur #date-boxes": "showTimeDiffInWords",
   },
 
   startTime: function() {
@@ -54,25 +62,44 @@ var SleepView = Backbone.View.extend({
     return new Date(year, month, day, hour, minute, 0, 0);
   },
 
-  calcTimeBetween: function() {
-    var start = this.startTime(), end = this.endTime();
-    var diff = this.endTime() - this.startTime();
-    //FIXME for negative numbers
-    // no integer division in javascript :(
-    var days = Math.floor(diff / 86400000);
-    var hours = Math.floor(diff / 3600000 % 24);
-    var minutes = Math.floor(diff / 60000 % 60);
-
-    var time_str = hours + " hours " + minutes + " minutes";
-    if (days) {
-      time_str = days + " days " + time_str;
-    }
-
-    this.time_between.text(time_str);
+  timeDiff: function() {
+    return this.endTime() - this.startTime();
   },
 
-  render: function() {
+  showTimeDiffInWords: function() {
+    //FIXME for negative numbers
+    // no integer division in javascript :(
+    var diff = this.timeDiff();
 
+    if (isNaN(diff)) {
+      this.time_between.text("Fix the bad input dates");
+    }
+    else {
+      var days = Math.floor(diff / 86400000);
+      var hours = Math.floor(diff / 3600000 % 24);
+      var minutes = Math.floor(diff / 60000 % 60);
+
+      var time_str = hours + " hours " + minutes + " minutes";
+      if (days) {
+        time_str = days + " days " + time_str;
+      }
+
+      this.time_between.text(time_str);
+    }
+  },
+
+  close: function() {
+    if (isNaN(diff)) {
+      return;
+    }
+
+    this.
+    
+    this.inputReset();
+  },
+
+  render: function(e) {
+    if (e.keyCode == 13) this.close();
   }
 });
 
