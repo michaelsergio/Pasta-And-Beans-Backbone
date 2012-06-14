8bb8581c7ab5f7f66e3f3f092e60f2b302230a0f
Michael Sergio <mikeserg@gmail.com>
2012-06-13 22:14:42 -0400

    added foods entry into app
    still need to add meals

---
+++ food.js
@@ -0,0 +1,68 @@
+var Food = Backbone.Model.extend({
+});
+
+var Foods = Backbone.Collection.extend({
+  model: Food,
+  localStorage: new Store("pastabeans-food")
+});
+
+var FoodAddView = Backbone.View.extend({
+  el: $('#food-add'),
+  events: {
+    "keypress": "newFood"
+  },
+
+  initialize: function(options) {
+    this.foods = options["collection"];
+  },
+
+  newFood: function(e) {
+    if (e.charCode == 13) {
+      var food = {};
+      _.each(this.$el.serializeArray(), function(obj) {
+        food[obj.name] = obj.value;
+      });
+      this.foods.create(food);
+      console.log(this.foods.length);
+    }
+  },
+
+});
+
+var FoodCheckItem = Backbone.View.extend({
+  template: function() {
+    return '<label class="checkbox"><input type="checkbox">' + 
+           this.model.get('name') + '</input></label>';
+  },
+
+  render: function() {
+    return this.template();
+  }
+
+});
+
+var FoodCheckList = Backbone.View.extend({
+  el: '#meal-foods',
+  initialize: function() {
+    this.collection.bind('reset', this.render, this);
+    this.collection.bind('add', this.render, this);
+    this.render();
+  },
+
+  render: function() {
+    var self = this;
+    self.$el.empty();
+    self.collection.each(function(food) {
+      var item = new FoodCheckItem({model:food});
+      self.$el.append(item.render());
+    });
+  }
+});
+
+this.foods = new Foods();
+this.foods.fetch({ 
+  success: function() {
+    if (!this.foodCheckList) this.foodCheckList = new FoodCheckList({collection: foods});
+    if (!this.foodAddView) this.foodAddView = new FoodAddView({collection: foods});
+  }
+});
---
+++ meals.js
@@ -0,0 +1,37 @@
+var Meal = Backbone.Model.extend({
+});
+
+var Meals = Backbone.Collection.extend({
+  model: Meal,
+  localStorage: new Store("pastabeans-meal")
+});
+
+var MealAddView = Backbone.View.extend({
+  el: $('#meal-add'),
+  events: {
+    "keypress": "newMeal"
+  },
+
+  initialize: function(options) {
+    this.meals = options["collection"];
+  },
+
+  newMeal: function(e) {
+    if (e.charCode == 13) {
+      var meal = {};
+      _.map(this.$('form'), function(name, val) {meal.name = meal.val });
+      this.meals.create(meal);
+      console.log(this.meals.length);
+    }
+  },
+
+});
+
+if (!this.meals) this.meals = new Meals();
+this.meals.fetch({ 
+  success: function() {
+    if (!this.mealAddView) {
+      this.mealAddView = new MealAddView({collection: meals});
+    }
+  }
+});
